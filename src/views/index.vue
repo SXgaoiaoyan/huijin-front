c3cefcbf71a330d282eff6af4866d10f6d6635bd
Your Name <you@example.com>
2018-07-11 14:19:04 +0800

    更改index

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,103 +1,406 @@
 <template>
-    <Table border :columns="columns7" :data="data6"></Table>
+	<div class="mall">
+		<!--头部-->
+	    <div class="header">
+	    	<Row>
+	    		<i-col span="1">&nbsp;</i-col>
+	    		<i-col span="3">
+	    			<img src="../static.huijinjiu.com/mall/LOGO.png" class="logo"/>
+	    		</i-col>
+	    		<i-col span="3">&nbsp;</i-col>
+	    		<i-col span="12">
+	    			<ul>
+	    				<i-col span="6">
+	    					<li><a href="">商城首页</a></li>
+	    				</i-col>
+	    				<i-col span="6">
+	    					<li><a href="">最新资讯</a></li>
+	    				</i-col>
+	    				<i-col span="6">
+	    					<li><a href="">酒品分类</a></li>
+	    				</i-col>
+	    				<i-col span="6">
+	    					<li><a href="">个人中心</a></li>
+	    				</i-col>
+	    			</ul>
+	    		</i-col>
+	    		<i-col span="5"></i-col>
+	    	</Row>
+	    </div>
+	    
+	    <!--内容1-->
+   		<div class="text1">
+   			<Row>
+	    		<i-col span="7" offset="1">
+	    			<div class="s1">
+	    				<span style="font-size: 58px;">汇金</span><span style="font-size: 42px;">原浆酒</span>
+	    				<br />
+	    				<br />
+	    				<ul>
+	    					<i-col span="6">&nbsp;</i-col>
+	    					<i-col span="6">
+	    						<li>芝香清冽</li>
+	    					</i-col>
+	    					<i-col span="6">
+	    						<li>醉时愉悦</li>
+	    					</i-col>
+	    					<i-col span="6">
+	    						<li>醒后喜悦</li>
+	    					</i-col>
+	    				</ul>
+	    				<br />
+	    				<br />
+	    				<ol>
+	    					<i-col span="12">&nbsp;</i-col>
+	    					<i-col span="6">
+	    						<li>十年陈酿</li>
+	    					</i-col>
+	    					<i-col span="6">
+	    						<li>惊世绽放</li>
+	    					</i-col>
+	    				</ol>
+	    				
+	    			</div>
+	    		</i-col>
+
+	    		<i-col span="14" offset="1">
+	    			<img src="../static.huijinjiu.com/mall/banner.png" class="img1"/>
+	    		</i-col>
+	    		<i-col span="1">&nbsp;</i-col>
+   			</Row>
+   		</div>
+   		
+   		<!--内容2-->
+   		<div class="text2">
+   			<Row>
+   				<!--中部-->
+   				<i-col span="12" offset="6">
+	   				<div class="hz">
+	   					<!--上-->
+						<i-col span="15" offset="5">
+							<span style="font-size: 34px;">洪</span>,<span style="font-size: 19px;">水大而宽广之意</span>。<span style="font-size: 34px;">泽</span>,<span style="font-size: 19px;">众水所聚集之地。</span>
+						</i-col>
+						<i-col span="4">&nbsp;</i-col>
+						<!--中--> 
+						<i-col span="6" offset="9" style="text-indent: -2em;">
+							<span style="font-size: 18px;">一个大湖,一瓶美酒。</span>
+						</i-col>
+						<i-col span="9">&nbsp;</i-col>	
+	   				</div>
+					
+					<!--下-->
+					<p class="bottom">
+						<i-col span="24" class="col1">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;洪泽湖,中国腹地最清澈的大湖,在中国以富庶知名的大省江苏。明清以来著名的鱼米之乡。有了上佳谷物,再
+						</i-col>
+						<i-col span="24" class="col1">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;凭借大湖湿地,产生了独特风格的白酒。湖畔众多饭馆食肆,少不了又美味的鱼禽佐酒,飘扬的酒旗连绵招展。
+						</i-col> 
+						<i-col span="24" class="col1">  									
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;淮扬水系便利,南来北往,文人墨客,大商巨贾,每每把洪泽湖所产的美酒,携往他乡。水与水是不同的,酒与
+						</i-col> 
+						<i-col span="24" class="col1">  									
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;酒是不同的。那大湖的上方,时常有云。云美好而多变,于光影间形成万千景致。酒自粮、水而变,正如云自大
+						</i-col> 
+						<i-col span="24" class="col1">
+							&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;湖所生。观云白酒,是以"观云"为名。来自洪湖的观云,是最清冽的浓香型白酒。观云带给你一个大湖上,   									
+						</i-col>
+						<i-col span="24" class="col1" style="text-indent: 13.5em;">
+							自然清新的气息,心旷神怡的视野,甚至,众流归宗的气象
+						</i-col> 		
+					</p>
+   						
+   				</i-col>
+	    		<i-col span="6">&nbsp;</i-col>
+   			</Row>
+   		</div>
+   		
+   		<!--内容3-->
+   		<div class="text3">
+   			<Row>
+   				<i-col span="3" offset="7">
+   					<img src="../static.huijinjiu.com/mall/wine.png" class="img2"/>
+   				</i-col>
+   				<i-col span="7" offset="3">
+   					<div class="gy">
+   						<Row>
+   							<i-col span="24">
+   								<span style="font-size: 23px;">内心</span><span style="font-size: 18px;">的变化已经发生,</span><span style="font-size: 23px;">外在</span><span style="font-size: 18px;">的变化必然出现.</span>  								
+   							</i-col>
+   						</Row>
+   						<div class="below">
+   							<i-col span="24">
+   								是的,这不像是一个酒瓶。至少不像中国白酒的酒瓶。那
+   							</i-col>
+   							<i-col span="24">
+   								是因为,此前所见所有的白酒酒瓶,设计上有难以摆脱的理
+   							</i-col>
+   							<i-col span="24">
+   								性。&nbsp;这个极度简洁的设计,&nbsp;出现第一天,&nbsp;就面对无数的质
+   							</i-col>
+   							<i-col span="24">
+   								疑。何况它的实现难度,&nbsp;跟完成一场革命一样。如你所见
+   							</i-col>
+   							<i-col span="24">
+   								&nbsp;,这个过于简单的设计,&nbsp;还是呈现在你面前了。&nbsp;设计是一
+   							</i-col>
+   							<i-col span="24">
+   								种语言,读懂它,&nbsp;并不难。
+   							</i-col>
+   							<i-col span="24">
+   								这瓶子,&nbsp;&nbsp;过于方正,&nbsp;&nbsp;过于剔透,&nbsp;&nbsp;过于棱角分明。&nbsp;&nbsp;正如你,								
+   							</i-col>
+   							<i-col span="24">   								
+   								过于直白,&nbsp;&nbsp;&nbsp;过于坦荡,&nbsp;&nbsp;过于清晰的性情。&nbsp;而那,&nbsp;才是,&nbsp;真
+   							</i-col>
+   							<i-col span="24">   								
+   								。性。情。
+   							</i-col>
+	   						<i-col span="24">
+	   							<button type="button" class="btn">立即购买</button>
+	   						</i-col>
+   						</div>
+   						
+   					</div>
+   				</i-col>
+   				<i-col span="4">&nbsp;</i-col>
+   			</Row>
+   		</div>
+   		
+   		<!--底部-->
+   		<div class="foot">
+   			<Row>
+   				<i-col span="7">
+   					<img src="../static.huijinjiu.com/mall/club.png"/>
+   				</i-col>	
+	   			<i-col span="12" class="col2">
+   					<i-col span="5">
+   						<button type="button">一<br />键<br />登<br />录<br</button>
+   					</i-col>
+   					<i-col span="5">
+   						<button type="button">多<br />客<br />服</button>
+   					</i-col>
+   					<i-col span="5">
+   						<button type="button">售<br />后<br />服<br />务</button>
+   					</i-col>
+   					<i-col span="5">
+   						<button type="button">售<br />后<br />服<br />务</button>
+   					</i-col>
+   					<i-col span="4">
+   						<button type="button">售<br />后<br />服<br />务</button>
+   					</i-col>
+	   				
+	   			</i-col>
+   				
+   				<i-col span="5">&nbsp;</i-col>
+   			</Row>
+   		</div>
+   		
+   		<!--脚部-->
+   		<div class="footer">
+   			<Row>
+   				<i-col span="6">
+   					<img src="../static.huijinjiu.com/mall/lotus.png" />
+   				</i-col>
+   				
+   				<i-col span="1">
+   					<ul>
+   						<li><a href="">关于我们</a></li>
+   						<li><a href="">联系我们</a></li>
+   						<li><a href="">加入我们</a></li>
+   					</ul>
+   					
+   				</i-col>
+   				<i-col span="4" offset="2">
+   					<p class="p1">注意未满十八岁,禁止购买!</p>
+   				</i-col>
+   				<i-col span="2" offset="3">
+   					<div class="gz">
+   						关注公众号
+	   					<div class="we">
+	   						加入我们
+	   					</div>
+   					</div>
+   				</i-col>
+   				<i-col span="2" offset="1">
+   					<img src="../static.huijinjiu.com/mall/code.png"  class="img3"/>
+   				</i-col>
+   				<i-col span="3">&nbsp;</i-col>
+   				
+   			</Row>
+   		</div>
+    </div>
 </template>
 <script>
     export default {
-        data () {
-            return {
-                columns7: [
-                    {
-                        title: 'Name',
-                        key: 'name',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('Icon', {
-                                    props: {
-                                        type: 'person'
-                                    }
-                                }),
-                                h('strong', params.row.name)
-                            ]);
-                        }
-                    },
-                    {
-                        title: 'Age',
-                        key: 'age'
-                    },
-                    {
-                        title: 'Address',
-                        key: 'address'
-                    },
-                    {
-                        title: 'Action',
-                        key: 'action',
-                        width: 150,
-                        align: 'center',
-                        render: (h, params) => {
-                            return h('div', [
-                                h('Button', {
-                                    props: {
-                                        type: 'primary',
-                                        size: 'small'
-                                    },
-                                    style: {
-                                        marginRight: '5px'
-                                    },
-                                    on: {
-                                        click: () => {
-                                            this.show(params.index)
-                                        }
-                                    }
-                                }, 'View'),
-                                h('Button', {
-                                    props: {
-                                        type: 'error',
-                                        size: 'small'
-                                    },
-                                    on: {
-                                        click: () => {
-                                            this.remove(params.index)
-                                        }
-                                    }
-                                }, 'Delete')
-                            ]);
-                        }
-                    }
-                ],
-                data6: [
-                    {
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park'
-                    },
-                    {
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park'
-                    },
-                    {
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park'
-                    },
-                    {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park'
-                    }
-                ]
-            }
-        },
-        methods: {
-            show (index) {
-                this.$Modal.info({
-                    title: 'User Info',
-                    content: `Name：${this.data6[index].name}<br>Age：${this.data6[index].age}<br>Address：${this.data6[index].address}`
-                })
-            },
-            remove (index) {
-                this.data6.splice(index, 1);
-            }
-        }
+        
     }
 </script>
+<style scoped>
+	.mall{
+		width: 100%;
+		background:url(../static.huijinjiu.com/mall/bg.png) no-repeat;
+		background-size: 100% 100%;
+	}
+	/*头部*/
+	.header{
+		width: 100%;
+		height: 120px;
+		box-shadow: 0 3px 8px #b5b5b5;
+	}
+	.header .logo{
+		margin-top: 32px;	
+	}
+	.header ul li{
+		list-style: none;
+		float: left;
+		font-size: 16px;
+		line-height: 116px;
+		letter-spacing: 2.5px;
+		
+	}
+	.header ul li a{
+		color: #010101;
+	}
+	.header ul li:hover{
+		border-bottom: 4px solid #a8d3fe;
+	}
+	/*内容1*/
+	.text1{
+		width: 100%;
+		margin-top: 50px;
+	}
+	.text1 .s1{
+		height: 154px;
+		margin-top: 144px;
+	}
+	.text1 ul li,.text1 ol li{
+		float: left;
+		list-style: none;
+		font-size: 17px;
+		color: #000000;
+	}
+	
+	.text1 .img1{
+		width: 100%;
+	}
+	/*内容2*/
+	.text2{
+		width: 100%;
+		/*height: 356px;*/
+		height: 482px;
+		margin-top: 90px;
+		background: url(../static.huijinjiu.com/mall/bird.png) no-repeat;
+		background-size: 100% 100%;
+	}
+	.text2 .hz{
+		color: #919594;
+		margin-top: 116px;
+		height: 62px;
+		text-align: center;
+	}
+	.text2 .bottom{
+		color: #919594;
+		height: 134px;
+		margin-top: 10px;
+	}
+	.text2 .bottom .col1{
+		margin-top: 4px;
+	}
+	/*内容3*/
+	.text3{
+		width: 100%;
+		background: url(../static.huijinjiu.com/mall/img.png) no-repeat;
+		background-size: 100% 100%;
+		margin-top: 108px;
+	}
+	.text3 .img2{
+		margin-top: 10px;
+	}
+	.text3 .gy{
+		width: 100%;
+		height: 360px;
+		margin-top: 82px;
+		color: #919594;
+	}
+	.text3 .gy .below{
+		margin-top: 38px;
+		font-size: 14px;
+	}
+	.text3 .gy .below .btn{
+		width: 134px;
+		height: 52px;
+		background-image: url(../static.huijinjiu.com/mall/btnborder.png);
+		background-repeat: no-repeat;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		color: red;
+	}
+	/*底部*/
+	.foot{
+		width: 100%;
+		height: 268px;
+		margin-top: 114px;
+	}
+	.foot button{
+		width: 60px;
+		height: 116px;
+		border: none;
+		outline: none;
+		background-color: transparent;
+		font-size: 16px;
+		color: #777777;
+		background: url(../static.huijinjiu.com/mall/border.png) no-repeat;
+	}
+	.foot .col2{
+		margin-top: 150px;
+	}
+	/*脚部*/
+	.footer{
+		width: 100%;
+		height: 248px;
+		margin-top: 168px;
+	}
+	.footer ul{
+		width: 70px;
+		height: 70px;
+		margin-top: 122px;
+	}
+	.footer ul li{
+		font-size: 16px;
+		list-style: none;
+	}
+	.footer  ul li a{
+		color: #838288;
+	}
+	.footer .p1{
+		color: #838288;
+		font-size: 16px;
+		margin-top: 156px;
+	}
+	.footer .gz{
+		font-size: 16px;
+		margin-top: 122px;
+	}
+	.footer .gz .we{
+		text-indent: 1em;
+		margin-top: 30px;
+	}
+	.footer .img3{
+		margin-top: 100px;
+		
+	}
+	@media screen and (max-width: 1920px){
+		.text2 .bottom {
+			font-size: 12px;
+			letter-spacing: 5px;
+		}
+	}
+	@media  screen and (max-width: 1366px) {
+		.text2 .bottom{
+			font-size: 12px;
+			letter-spacing: 0.6px;
+		}
+	}
+</style>
