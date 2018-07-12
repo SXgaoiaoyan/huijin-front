68b1e17f0b82ebfd5c2351d38f6c9d0b58867a41
Your Name <you@example.com>
2018-07-12 16:15:47 +0800

    build

--- src/views/find_user.vue
+++ src/views/find_user.vue
@@ -10,28 +10,28 @@ export default {
     var self = this;
     var token=this.$route.query.token;
     this.ajax.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-    this.ajax
-      .get("/api/userinfo")
-      .then(response => {
-        var data={}
-        data.token=token
-        data.user=response.data.user
-        self.$store.commit(
-          "login",
-          data
-        );
-        self.$router.push({
-          name: "home"
-        });
-      })
-      .catch(error => {
-        if (error.status_code == 403) {
-          self.$Message.error(error.message);
-        }
-        // self.$router.push({
-        //   name: "login"
-        // });
-      });
+    // this.ajax
+    //   .get("/api/userinfo")
+    //   .then(response => {
+    //     var data={}
+    //     data.token=token
+    //     data.user=response.data.user
+    //     self.$store.commit(
+    //       "login",
+    //       data
+    //     );
+    //     self.$router.push({
+    //       name: "home"
+    //     });
+    //   })
+    //   .catch(error => {
+    //     if (error.status_code == 403) {
+    //       self.$Message.error(error.message);
+    //     }
+    //     // self.$router.push({
+    //     //   name: "login"
+    //     // });
+    //   });
   }
 };
 </script>>
