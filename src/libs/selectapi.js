69d21c4b9aac07d76bcf4c197d04469e2c46d62a
Your Name <you@example.com>
2018-07-24 13:46:36 +0800

    update .gitignore

--- src/libs/selectapi.js
+++
@@ -1,9 +0,0 @@
-import env from '../config/env';
-
-let api= env === 'development' ?
-    'http://api.huijinjiu.com' :
-    env === 'production' ?
-    'http://api.huijinjiu.com' :
-    'https://debug.url.com';
-
-export default api;
