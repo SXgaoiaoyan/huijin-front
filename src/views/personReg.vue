5392051606397f328a40cffb35608db2fe73aa6f
Your Name <you@example.com>
2018-07-26 11:30:12 +0800

    perreg

--- src/views/personReg.vue
+++ src/views/personReg.vue
@@ -25,45 +25,60 @@
             </Steps>
             <!-- 验证手机号 -->
             <div style="width:420px;margin-left:auto;margin-right:auto" v-show="current==0">
-              <Form>
-                <FormItem>
-                  <Input type="text" placeholder="请输入常用手机号" class="phone" clearable size="large">
+              <Form ref="phoneFormValidate" :model="phoneFormValidate" :rules="phoneRuleValidate">
+                <FormItem prop='phone_d'>
+                  <Input type="text" v-model="phoneFormValidate.phone_d" placeholder="请输入常用手机号" style="margin-top:20px" clearable size="large">
                   <span slot="prepend"> 中国+86 </span>
                   </Input>
                 </FormItem>
+                <FormItem v-show="phoneFormValidate.phone_d.length==11" style="margin:0;margin-bottom:18px">
+                  <div class="l-captcha" data-site-key="a61ebded8b92ba71b5272a5f60fc1be7" data-callback='getCaptchaResponse'></div>
+                </FormItem>
+                <FormItem prop='code_d'>
+                  <Input type="text" placeholder="请输入短信验证码" clearable size="large" v-model="phoneFormValidate.code_d">
+                  <span slot="prepend">手机验证码</span> -->
+                  <!-- <span  slot="append"  style="cursor:pointer">请先进行人机验证</span> -->
 
-                <FormItem>
-                  <Checkbox>
+                  <span v-show='!checked_d' slot="append" @click='send_code_m' style="cursor:pointer">请先进行人机验证</span>
+                  <span v-show='checked_d&&!sended_d' slot="append" @click='send_code_m' style="cursor:pointer">发送验证码</span>
+                  <span v-show='sended_d' slot="append" style="cursor:pointer">{{time_d}}s后重新获取</span>
+                  <!-- <span  slot="append" style="cursor:pointer">s后重新获取</span> -->
+                  </Input>
+                </FormItem>
+
+                <FormItem prop='confirmation'>
+                  <Checkbox v-model="phoneFormValidate.confirmation">
                     我已阅读并同意
                     <a>
                       <<喜马拉雅用户注册协议和隐私条款>> </a>
                   </Checkbox>
-                  <Button class='login' @click="next_m">
-                    下一步
-                  </Button>
-                  <div style="margin-top:15px">
-                    <router-link to="/companyReg"><img src="../static.huijinjiu.com/companyReg/user.png" style="vertical-align:middle" />
-                      <span style="color:black">企业用户注册</span>
-                    </router-link>
-                  </div>
+                </FormItem>
+                <Button type="primary" class='login' @click="phone_login_m('phoneFormValidate')">
+                  下一步
+                </Button>
+                <div style="margin-top:15px">
+                  <router-link to="/companyReg"><img src="../static.huijinjiu.com/companyReg/user.png" style="vertical-align:middle" />
+                    <span style="color:black">企业用户注册</span>
+                  </router-link>
+                </div>
                 </FormItem>
               </Form>
             </div>
             <!-- 填写账号信息 -->
             <div style="width:420px;margin-left:auto;margin-right:auto" v-show="current==1">
-              <Form>
-                <FormItem style="margin-top:20px">
-                  <Input type="text" size="large" placeholder="设置数字与字母的密码">
+              <Form ref="formCustom" :model="formCustom" :rules="ruleCustom">
+                <FormItem prop="passwd">
+                  <Input type="password" v-model="formCustom.passwd">
                   <span slot="prepend">设置密码</span>
                   </Input>
                 </FormItem>
-                <FormItem>
-                  <Input type="text" size="large" placeholder="再次输入密码">
+                <FormItem prop="passwdCheck">
+                  <Input type="password" v-model="formCustom.passwdCheck">
                   <span slot="prepend">确认密码</span>
                   </Input>
                 </FormItem>
                 <FormItem>
-                  <Button class="login" @click="twoStep_m">下一步</Button>
+                  <Button class="login" @click="bindPassword('formCustom')">下一步</Button>
                 </FormItem>
               </Form>
               <div style="margin-top:15px">
@@ -137,24 +152,184 @@
 <script>
 export default {
   data() {
+    const validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        if (this.formCustom.passwdCheck !== "") {
+          // 对第二个密码框单独验证
+          this.$refs.formCustom.validateField("passwdCheck");
+        }
+        callback();
+      }
+    };
+    const validatePassCheck = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.formCustom.passwd) {
+        callback(new Error("这两次输入的密码不一致!"));
+      } else {
+        callback();
+      }
+    };
     return {
-      current: 0
+      current: 0,
+      //验证码发送等所需数据
+      time_d: 60,
+      phone_login_d: true,
+      checked_d: false,
+      sended_d: false,
+
+      phoneFormValidate: {
+        phone_d: "",
+        code_d: "",
+        confirmation: ""
+      },
+      phoneRuleValidate: {
+        phone_d: [
+          { required: true, message: "手机号码不能为空", trigger: "blur" },
+          {
+            type: "string",
+            len: 11,
+            message: "请输入11位手机号",
+            trigger: "blur"
+          }
+        ],
+        code_d: [
+          { required: true, message: " 验证码不能为空", trigger: "blur" },
+          {
+            type: "string",
+            len: 4,
+            message: "请输入四位验证码",
+            trigger: "blur"
+          }
+        ],
+        confirmation: [
+          {
+            type: "boolean",
+            value: true,
+            message: "请点击同意条款",
+            trigger: "change"
+          }
+        ]
+      },
+      //2.绑定密码页面验证数据及规则
+      formCustom: {
+        passwd: "",
+        passwdCheck: ""
+      },
+      ruleCustom: {
+        passwd: [{ validator: validatePass, trigger: "blur" }],
+        passwdCheck: [{ validator: validatePassCheck, trigger: "blur" }]
+      }
     };
   },
+  mounted() {
+    var SCRIPT_URL = `//captcha.luosimao.com/static/dist/api.js`;
+    var scriptHeat = document.createElement("script");
+    scriptHeat.type = "text/javascript";
+    scriptHeat.src = SCRIPT_URL;
+    scriptHeat.onload = onload;
+    document.body.appendChild(scriptHeat);
+    window.getCaptchaResponse = this.getCaptchaResponse;
+  },
   methods: {
-    next_m() {
-      if (this.current == 2) {
-        this.current = 0;
-      } else {
-        this.current += 1;
-      }
+    phone_login_m(name) {
+      var self = this;
+
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          this.$Message.success("登陆成功");
+          self.ajax
+            .post("/api/login", {
+              phone: self.phoneFormValidate.phone_d,
+              code: self.phoneFormValidate.code_d
+            })
+            .then(response => {
+              self.$store.commit("login", response.data);
+              self.current += 1;
+            })
+            .catch(error => {
+              if (error.status_code == 403) {
+                self.$Message.error(error.message);
+                LUOCAPTCHA.reset();
+              }
+            });
+        } else {
+          this.$Message.error("登录失败");
+        }
+      });
     },
-    twoStep_m() {
-      if (this.current == 2) {
-        this.current = 0;
-      } else {
-        this.current += 1;
-      }
+     bindPassword(name) {
+      // this.current += 1;
+      var self=this
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          self.ajax
+            .post("/api/updateCompanyPassword", {
+              password: self.formCustom.passwd,
+            })
+            .then(response => {
+              self.current += 1;
+              self.$Message.success("登陆成功");
+            })
+            .catch(error => {
+              if (error.status_code == 422) {
+                self.$Message.error(error.message);
+              }
+            });
+        } else {
+          this.$Message.error("Fail!");
+        }
+      });
+    },
+    //人机验证成功返回
+    getCaptchaResponse(resp) {
+      var self = this;
+      this.ajax
+        .post("/api/checkcaptcha", {
+          captcha: resp
+        })
+        .then(function(response) {
+          self.checked_d = true;
+        })
+        .catch(function(error) {
+          if (error.status_code == 400) {
+            alert(error.message);
+            LUOCAPTCHA.reset();
+          }
+        });
+    },
+    send_code_m() {
+      var self = this;
+
+      this.ajax
+        .post("/api/sendcode", {
+          phone: self.phoneFormValidate.phone_d
+        })
+        .then(function(response) {
+          self.Interval();
+          self.sended_d = true;
+        })
+        .catch(function(error) {
+          if (error.status_code == 400) {
+            alert(error.message);
+          }
+        });
+    },
+    Interval() {
+      var self = this;
+      var fun = function() {
+        self.time_d--;
+        if (self.time_d == 0) {
+          clearInterval(interval);
+          self.time_d = 60;
+          self.checked_d = false;
+          self.sended_d = false;
+          LUOCAPTCHA.reset();
+        }
+      };
+      var interval = setInterval(fun, 1000);
     }
   }
 };
